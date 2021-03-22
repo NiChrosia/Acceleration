346d0eb92028c3f7e7f3ca609b78c3a9b8169613
NiChrosia <nichrosian@gmail.com>
2021-03-22 12:49:45 -0500

    Now functional refractive walls!

--- scripts/lib/reflective-wall.js
+++ scripts/lib/reflective-wall.js
@@ -16,31 +16,43 @@ function reflectiveWall(name, chancePercent, attributes) {
 				let bulletCollisionLength = Damage.findLaserLength(bullet, bullet.type.length);
 				let bulletVec = Vec2();
 				bulletVec.trns(bullet.rotation(), bulletCollisionLength);
-				let collisionX = bullet.x + bulletVec.x;
-				let collisionY = bullet.y + bulletVec.y;
+				let collision = {};
+				collision.x = bullet.x + bulletVec.x;
+				collision.y = bullet.y + bulletVec.y;
 				
 				// Angle to bullet collision
-				let angle = this.angleTo(collisionX, collisionY);
+				let angle = this.angleTo(collision.x, collision.y);
 				
 				// Calculate rotation to bullet
 				let rotation = Mathf.round(angle / 90)
 				
 				// Calculate directions
-				let left = rotation == 2;
-				let right = rotation == 0;
-				let bottom = rotation == 3;
-				let top = rotation == 1;
+				let left = bullet.x < this.x;
+				let right = bullet.x > this.x;
+				let bottom = bullet.y < this.y;
+				let top = bullet.y > this.y;
 				
 				// Calculate difference in x and y
-				let diffX = (left ? bullet.x - collisionX : (right ? collisionX - bullet.x : 0)) * Vars.tilesize
-				let diffY = (bottom ? bullet.y - collisionY : (top ? collisionY - bullet.y : 0)) * Vars.tilesize
+				let diffX = Math.abs(bullet.x - collision.x)
+				let diffY = Math.abs(bullet.y - collision.y)
+				
+				let refractX = bullet.x + diffX * (right ? -1 : 1);
+				let refractY = bullet.y + diffY * (top ? -1 : 1);
+				
+				print("[accent]-==========================================================================-[]")
+				print("[accent]Left:[] " + left + ", [accent]Right:[] " + right + ", [accent]Bottom:[] " + bottom + ", [accent]Top:[] " + top)
+				print("[accent]DiffX:[] " + (diffX) + ", [accent]DiffY:[] " + (diffY))
+				print("[accent]BulletX:[] " + (bullet.x) + ", [accent]BulletY:[] " + (bullet.y))
+				print("[accent]X:[] " + refractX + ", [accent]Y:[] " + refractY)
+				Fx.explosion.at(refractX, refractY)
+				Fx.commandSend.at(refractX, refractY)
 				
 				// Get new angle
-				let refractAngle = this.angleTo(bullet.x + diffX, bullet.y + diffY)
+				let refractAngle = this.angleTo(refractX, refractY)
 				print(refractAngle)
 				
 				// Refract the bullet
-				const shoot = bullet.type != null ? () => {bullet.type.create({}, this.team, collisionX, collisionY, refractAngle)} : () => {}
+				const shoot = bullet.type != null ? () => {bullet.type.create({}, this.team, collision.x, collision.y, refractAngle)} : () => {}
 				Vars.state.paused ? null : Timer.schedule(shoot, 0.1)
 			}
 			return !pierce
