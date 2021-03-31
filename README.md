ee452034f1b907f18ee6d4c621feb809fb939b01
NiChrosia <nichrosian@gmail.com>
2021-03-31 16:43:14 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Acceleration - Kotlin
+
+A modified version of Acceleration using Kotlin. Will likely be the official version later.  
+Created using Anuken's ExampleKotlinMod
---
+++ src/acceleration/Acceleration.kt
@@ -0,0 +1,14 @@
+package example
+
+import arc.Events
+import arc.util.*
+import mindustry.game.EventType.*
+import mindustry.mod.Mod
+import mindustry.ui.dialogs.*
+
+class Acceleration : Mod(){
+
+    override fun loadContent(){
+        Log.info("Mod [accent]Acceleration [sky]Kotlin[] loaded content successfully.")
+    }
+}
