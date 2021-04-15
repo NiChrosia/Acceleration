252b9033b3c77f183de5e838ae888bfa504193d3
NiChrosia <nichrosian@gmail.com>
2021-04-15 14:22:18 -0500

    Add division of resources depending on the amount of nearby Collectors.

--- src/acceleration/world/blocks/units/Collector.kt
+++ src/acceleration/world/blocks/units/Collector.kt
@@ -1,9 +1,11 @@
 package acceleration.world.blocks.units
 
 import arc.Events
+import arc.math.geom.Geometry
 import arc.struct.ObjectMap
 import arc.struct.Seq
 import arc.util.Log
+import arc.util.io.Reads
 import mindustry.Vars
 import mindustry.game.EventType
 import mindustry.gen.Building
@@ -22,6 +24,7 @@ import mindustry.world.modules.ItemModule
 
 open class Collector(name: String) : Block(name) {
     private val unitMap = ObjectMap<UnitType, Array<out ItemStack>>()
+    private val collectors = Seq<CollectorBuild>()
 
     val range = 120f
     val tier = 5f
@@ -49,8 +52,6 @@ open class Collector(name: String) : Block(name) {
                 }
             }
         }
-
-        // TODO make collector produce resources from all of the reconstructors, rather than just the last one.
     }
 
     override fun drawPlace(x: Int, y: Int, rotation: Int, valid: Boolean) {
@@ -65,19 +66,31 @@ open class Collector(name: String) : Block(name) {
             super.created()
 
             if (items == null) items = ItemModule()
-            Events.on(EventType.UnitDestroyEvent::class.java) { e -> units.add(e.unit) }
+            Events.on(EventType.UnitDestroyEvent::class.java) { e ->
+                if (e.unit.dst(x, y) < range) {
+                    units.add(e.unit)
+                }
+            }
+
+            collectors.add(this)
+        }
+
+        override fun read(read: Reads, revision: Byte) {
+            super.read(read, revision)
+
+            collectors.add(this)
+        }
+
+        override fun onRemoved() {
+            super.onRemoved()
+
+            collectors.remove(this)
         }
 
         override fun updateTile() {
             super.updateTile()
 
             updateItems()
-
-            Log.info("-")
-            Log.info(unitMap)
-            Log.info(units)
-            Log.info(queuedItems)
-            Log.info(items)
 
             queuedItems.each { i ->
                 for (iterItem in i.iterator()) {
@@ -103,7 +116,13 @@ open class Collector(name: String) : Block(name) {
                     val item = iterItem.item
                     val amount = iterItem.amount
 
-                    val percentAmount = (amount * (percent / 100)).toInt()
+                    var divideAmount = 1
+
+                    collectors.each { c -> if (c.dst(x, y) <= range && !(c.x == x && c.y == y)) divideAmount++ }
+
+                    if (divideAmount > 1) divideAmount -= (divideAmount / 2)
+
+                    val percentAmount = ((amount * (percent / 100)) / divideAmount).toInt()
 
                     queuedItems.add(ItemStack.with(item, percentAmount))
                 }
