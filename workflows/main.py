2d6c316c9b3a2a67c87ce475c6bb697a6665c675
NiChrosia <nichrosian@gmail.com>
2021-05-24 14:52:53 -0500

    Add python script to do string operations

---
+++ workflows/main.py
@@ -0,0 +1,14 @@
+import sys
+import json
+
+
+def convert_string(sha_url):
+    return f"https://github.com/NiChrosia/Acceleration/tree/{sha_url.split('/')[-1]}"
+
+
+if __name__ == '__main__':
+    string = convert_string(*sys.argv[0:])
+
+    with(open("output.json", "w")) as f:
+        json.dump(string, f, indent=4)
+        f.close()
