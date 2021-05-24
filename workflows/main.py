f7bebdc51f36d530d3227fc406dcd9a58eaaf3e9
NiChrosia <nichrosian@gmail.com>
2021-05-24 15:56:34 -0500

    Add more python code, test JSON

--- workflows/main.py
+++ workflows/main.py
@@ -1,14 +1,36 @@
 import sys
+import subprocess
 import json
 
 
-def convert_string(sha_url):
-    return f"https://github.com/NiChrosia/Acceleration/tree/{sha_url.split('/')[-1]}"
+def get_commit_name(hash):
+    result = subprocess.run(f"git show -s --format=%s {hash}", stdout=subprocess.PIPE)
+    return result.stdout
+
+
+def get_info(hash):
+    result = subprocess.run(f"git show {hash}", stdout=subprocess.PIPE).stdout.decode("utf-8")
+    author = result.splitlines(False)[1]
+    date = result.splitlines(False)[2]
+
+    return {
+        "full": result,
+        "author": author,
+        "date": date
+    }
 
 
 if __name__ == '__main__':
-    string = convert_string(*sys.argv[1:])
+    commit_name = get_commit_name(*sys.argv[1:]).decode("utf-8")
+    info = get_info(*sys.argv[1:])
 
     with(open("output.json", "w")) as f:
-        json.dump(string, f, indent=4)
+        data = {
+            "commit_name": commit_name,
+            "info": info["full"],
+            "author": info["author"],
+            "date": info["date"]
+        }
+
+        json.dump(data, f, indent=4)
         f.close()
