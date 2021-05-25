f5d9c1b4a25c1624af04b87d37ff965c72408e9d
NiChrosia <nichrosian@gmail.com>
2021-05-25 13:07:47 -0500

    Improve formatting

--- workflows/main.kts
+++ workflows/main.kts
@@ -1,5 +1,6 @@
 import java.io.File
 import java.util.concurrent.TimeUnit
+import kotlin.math.round
 
 fun String.runCommand(
     workingDir: File = File("."),
@@ -12,9 +13,117 @@ fun String.runCommand(
         .inputStream.bufferedReader().readText()
 }.onFailure { it.printStackTrace() }.getOrNull()
 
+data class Date(val source: String, val use24Hr: Boolean) {
+    val days = mapOf(
+        "Sun" to "Sunday",
+        "Mon" to "Monday",
+        "Tue" to "Tuesday",
+        "Wed" to "Wednesday",
+        "Thu" to "Thursday",
+        "Fri" to "Friday",
+        "Sat" to "Saturday"
+    )
+
+    val months = mapOf(
+        "Jan" to "January",
+        "Feb" to "February",
+        "Mar" to "March",
+        "Apr" to "April",
+        "May" to "May",
+        "Jun" to "June",
+        "Jul" to "July",
+        "Aug" to "August",
+        "Sep" to "September",
+        "Oct" to "October",
+        "Nov" to "November",
+        "Dec" to "December"
+    )
+
+    val monthdayEnds = mapOf(
+        1 to "st",
+        2 to "nd",
+        3 to "rd",
+        4 to "th",
+        5 to "th",
+        6 to "th",
+        7 to "th",
+        8 to "th",
+        9 to "th",
+        0 to "th"
+    )
+
+    /** Tue */
+    var weekday: String
+
+    /** May */
+    var month: String
+
+    /** 25 */
+    var monthday: Int
+
+    /** 12 */
+    var hour: Int
+
+    /** 16 */
+    var minute: Int
+
+    /** 34 */
+    var second: Int
+
+    /** 2021 */
+    var year: Int
+
+    var formatted: String
+    
+    fun convert24HrTo12Hr(hour: Int, minute: Int, second: Int): String {
+        /** Example:
+         * 12:59:59
+         * 12:60 AM
+         **/
+
+        val morning = hour < 12 || hour == 24
+
+        var outMinute = minute + round(second / 30.0).toInt()
+        val outHour = (hour - if (hour > 12) 12 else 0) + (if (outMinute == 60) 1 else 0)
+
+        if (outMinute == 60) outMinute %= 60
+
+        return "$outHour:$outMinute ${if (morning) "AM" else "PM"}"
+    }
+
+    fun convertDateToString(): String {
+        /** Monday, January 1st, 2021 */
+        return "${days[weekday]}, ${months[month]}${monthdayEnds[monthday % 10]} $monthday, $year"
+    }
+
+    init {
+        if (source.split(" ").size != 5) throw Exception("Source size must be 5, but found ${source.split(" ").size}")
+
+        /** Example:
+         * Tue May 25 12:16:34 2021
+         **/
+
+        val arr = source.split(" ")
+
+        weekday = arr[0]
+        month = arr[1]
+        monthday = arr[2].toInt()
+
+        val time = arr[3].split(":")
+
+        hour = time[0].toInt()
+        minute = time[1].toInt()
+        second = time[2].toInt()
+
+        year = arr[4].toInt()
+
+        formatted = "${if (use24Hr) convert24HrTo12Hr(hour, minute, second) else "$hour:$minute:$second"}, ${convertDateToString()}"
+    }
+}
+
 data class CommitInfo(val hash: String) {
     var author: String
-    var date: String
+    var date: Date
     var name: String
 
     init {
@@ -31,12 +140,10 @@ data class CommitInfo(val hash: String) {
              */
 
             author = process[1].split(" ").take(2).joinToString(" ") // Censor email
-            date = process[2]
+            date = Date(process[2].split(" ").take(6).joinToString(" "), true)
             name = process[4].substring(4)
         } else {
-            author = ""
-            name = ""
-            date = ""
+            throw NullPointerException("Result from command 'git show $hash' was null.")
         }
     }
 }
@@ -45,7 +152,7 @@ fun generateDescription(info: CommitInfo): String {
     return """## Commit information:
 **Name:** ${info.name}
 **Author:** ${info.author}
-**Date:** ${info.date}
+**Date:** ${info.date.formatted}
 ## Links:
 [*Filetree*](https://github.com/NiChrosia/Acceleration/tree/${info.hash})
 [*Commit*](https://github.com/NiChrosia/Acceleration/commit/${info.hash})"""
