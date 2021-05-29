44273409f124040512e37079363a01cfceefdd09
nichrosia <nichrosian@gmail.com>
2021-05-28 20:40:23 -0500

    Fix MUFD to function correctly, add icons

--- src/acceleration/ui/dialogs/ModularUnitFactoryDialog.kt
+++ src/acceleration/ui/dialogs/ModularUnitFactoryDialog.kt
@@ -9,17 +9,34 @@ import mindustry.ui.dialogs.BaseDialog
 
 class ModularUnitFactoryDialog : BaseDialog("modular-unit-factory", Styles.defaultDialog) {
     init {
+        addCloseListener()
+
         buttons.button("@back", Icon.left) { hide() }.name("back").size(160f, 64f)
 
         cont.table(Tex.button) { table -> table.apply {
-            pane {
-                it.button({b -> b.image(Core.atlas.find("")).size(42f).scaling(Scaling.fit)}, Styles.clearTransi) {
+                table(Tex.button) { table ->
+                    table.apply {
+                        pane {
+                            it.button(
+                                { b -> b.image(Core.atlas.find("")).size(42f).scaling(Scaling.fit) },
+                                Styles.clearTransi
+                            ) {
 
-                }.size(48f).pad(3f)
-            }.size(200f, 400f).growX()
+                            }.size(48f).pad(3f)
+                        }.size(200f, 400f).growX().left()
+                    }
+                }.size(250f, 500f).left().pad(10f)
 
-            table(Tex.button) {}.size(200f, 200f).right().top().pad(10f)
-            table(Tex.button) {}.size(200f, 200f).right().bottom().pad(10f)
-        }}.size(500f, 500f).left().pad(10f)
+                table { table ->
+                    table.apply {
+                        table(Tex.button) {}.size(250f, 250f).top().right().pad(10f)
+
+                        row()
+
+                        table(Tex.button) {}.size(250f, 250f).bottom().right().pad(10f)
+                    }
+                }.size(250f, 500f).right().pad(10f)
+            }
+        }.size(550f, 550f)
     }
 }
