abe82c22f5b0964b6ab6944bb84ed1e2e8250d8b
nichrosia <nichrosian@gmail.com>
2021-05-31 18:01:19 -0500

    Add reset button, add features to ViewModuleDialog, add sprites

--- src/acceleration/ui/dialogs/modularunit/FactoryDialog.kt
+++ src/acceleration/ui/dialogs/modularunit/FactoryDialog.kt
@@ -1,8 +1,12 @@
 package acceleration.ui.dialogs.modularunit
 
 import acceleration.Acceleration
+import acceleration.type.modularunit.ModularUnitProperties
 import arc.Core
 import arc.graphics.Color
+import arc.scene.Element
+import arc.scene.event.InputEvent
+import arc.scene.event.InputListener
 import arc.scene.ui.Image
 import arc.scene.ui.layout.Scl
 import arc.scene.ui.layout.Table
@@ -13,31 +17,56 @@ import mindustry.ui.Styles
 import mindustry.ui.dialogs.BaseDialog
 
 open class FactoryDialog : BaseDialog(Core.bundle.format("dialog.modular-unit-factory.name"), Styles.defaultDialog) {
+    protected var moduleProperties = Acceleration.modularUnitProperties
+
     init {
-        this.addCloseListener()
-        this.addCloseButton()
-
-        cont.table(Tex.button) { table -> table.apply {
-                table(Tex.button) { modules -> modules.apply {
-                    buildModules(this)
-                }}.size(500f, 800f).left()
-
-                table { table -> table.apply {
-                    table(Tex.button) { properties -> properties.apply {
-                        buildProperties(this)
-                    }}.size(500f, 400f).top()
-
-                    row()
-
-                    table(Tex.button) { installedModules -> installedModules.apply {
-                        buildInstalledModules(this)
-                    }}.size(500f, 400f).bottom()
-                }}.size(500f, 800f).right()
-            }
-        }.size(1050f, 850f)
+        rebuild()
     }
 
-    private fun buildModules(table: Table) { table.apply {
+    fun rebuild() {
+        this.addCloseListener()
+        this.addCloseButton()
+        this.addResetButton()
+
+        cont.table(Tex.button) { table -> table.apply {
+            table(Tex.button) { modules -> modules.apply {
+                buildModules(this)
+            }}.size(500f, 800f).left()
+
+            table { table -> table.apply {
+                table(Tex.button) { properties -> properties.apply {
+                    buildProperties(this)
+                }}.size(500f, 400f).top()
+
+                row()
+
+                table(Tex.button) { installedModules -> installedModules.apply {
+                    buildInstalledModules(this)
+                }}.size(500f, 400f).bottom()
+            }}.size(500f, 800f).right()
+        }
+        }.size(1050f, 850f)
+    }
+    
+    protected fun addResetButton() {
+        buttons.defaults().size(210f, 64f)
+        buttons.button(Core.bundle.format("button.modular-unit-reset.name"), Icon.refresh) {
+            Acceleration.modularUnitProperties = ModularUnitProperties()
+            moduleProperties = Acceleration.modularUnitProperties
+        }.name("clear").get().addListener(object : InputListener() {
+            /** When the mouse hovers over the apply button */
+            override fun enter(event: InputEvent?, x: Float, y: Float, pointer: Int, fromActor: Element?) {
+                moduleProperties = ModularUnitProperties()
+            }
+
+            /** When the mouse moves off of the apply button */
+            override fun exit(event: InputEvent?, x: Float, y: Float, pointer: Int, toActor: Element?) {
+                moduleProperties = Acceleration.modularUnitProperties
+            }
+        })
+    }
+
+    protected fun buildModules(table: Table) { table.apply {
         pane { modules -> modules.apply {
             Acceleration.modularUnitModules.each { module ->
                 button({ button -> button.apply {
@@ -63,17 +92,21 @@ open class FactoryDialog : BaseDialog(Core.bundle.format("dialog.modular-unit-fa
         }}.size(400f, 800f).growX().left()
     }}
 
-    private fun buildProperties(table: Table) { table.apply {
-        Acceleration.modularUnitProperties.each { prop ->
-            add(Bar(prop.name, prop.color) {
-                prop.fraction
-            }).size(400f, 40f)
+    protected fun buildProperties(table: Table) { table.apply {
+        clear()
 
-            row()
-        }
+        table { bars -> bars.apply {
+            moduleProperties.eachIndexed { index, prop ->
+                add(Bar(prop.name, prop.color) {
+                    moduleProperties[index].fraction
+                }).size(400f, 40f)
+
+                row()
+            }
+        }}
     }}
 
-    private fun buildInstalledModules(table: Table) { table.apply {
+    protected fun buildInstalledModules(table: Table) { table.apply {
         clear()
 
         pane { modules -> modules.apply {
@@ -82,17 +115,22 @@ open class FactoryDialog : BaseDialog(Core.bundle.format("dialog.modular-unit-fa
                     table { icon -> icon.apply {
                         center()
                         image(module.icon).size(32f, 32f).tooltip(module.name)
+
                         visible { Acceleration.modularUnitProperties.hasModule(module) }
+                        setDisabled { !Acceleration.modularUnitProperties.hasModule(module) }
+
                         pack()
 
                         table { numeral -> numeral.apply {
                             top().right()
                             label {Acceleration.modularUnitProperties.getModule(module)?.level?.toString() ?: ""}.fontScale(0.5f)
+
                             visible { Acceleration.modularUnitProperties.hasModule(module) }
+                            setDisabled { !Acceleration.modularUnitProperties.hasModule(module) }
                         }}
                     }}
                 }}, Styles.accenti) {
-                    Acceleration.ui.modularUnitViewModule.show(module)
+                    Acceleration.ui.modularUnitViewModule.show(Acceleration.modularUnitProperties.getModule(module) ?: module)
                 }
             }
         }}.size(400f, 800f).growX().left()
--- src/acceleration/ui/dialogs/modularunit/ViewModuleDialog.kt
+++ src/acceleration/ui/dialogs/modularunit/ViewModuleDialog.kt
@@ -2,20 +2,126 @@ package acceleration.ui.dialogs.modularunit
 
 import acceleration.type.modularunit.ModularUnitModule
 import arc.Core
+import arc.graphics.Color
 import arc.scene.ui.Dialog
+import arc.scene.ui.Image
+import arc.scene.ui.layout.Cell
+import arc.scene.ui.layout.Table
+import mindustry.gen.Tex
+import mindustry.graphics.Pal
+import mindustry.ui.Bar
+import mindustry.ui.Cicon
+import mindustry.ui.Styles
 import mindustry.ui.dialogs.BaseDialog
 
-open class ViewModuleDialog : BaseDialog(Core.bundle.format("dialog.modular-unit-view-module.name")) {
+open class ViewModuleDialog : BaseDialog(Core.bundle.format("dialog.modular-unit-view-module.name"), Styles.defaultDialog) {
     private var module: ModularUnitModule? = null
 
+    fun Table.line(pad: Float, height: Float, color: Color): Cell<*> {
+        return image().growX().pad(pad).padLeft(0f).padRight(0f).height(height).color(color)
+    }
+
+    fun Table.row(index: Int, start: Int = 0) {
+        if (index > start) row()
+    }
+
     init {
+        rebuild()
+    }
+
+    fun rebuild() {
+        buttons.clearChildren()
+        cont.clearChildren()
+
         this.addCloseListener()
         this.addCloseButton()
+
+        cont.table(Tex.button) { main -> main.apply {
+            table(Tex.button) { description -> description.apply {
+                buildDescription(this)
+            }}.size(500f, 600f)
+
+            table(Tex.button) { bars -> bars.apply {
+                buildBars(this)
+            }}.size(500f, 600f)
+        }}.size(1050f, 650f)
     }
 
     fun show(module: ModularUnitModule): Dialog? {
         this.module = module
 
+        rebuild()
+
         return show()
     }
+
+    fun buildDescription(table: Table) { table.apply {
+        module?.let { module ->
+            table { icons ->
+                icons.apply {
+                    add(Image(module.icon)).size(64f, 64f)
+                    add(module.name, module.color, 1.1f)
+                }
+            }.pad(5f)
+
+            row()
+
+            add(module.description, 0.85f).growX().wrap()
+
+            row()
+
+            table { stats ->
+                stats.apply {
+                    add("Stats: ", Pal.accent, 1.1f)
+
+                    row()
+
+                    line(5f, 3f, Pal.accent)
+
+                    row()
+
+                    add("Requirements: ", Pal.accent)
+
+                    row()
+
+                    table { requirements ->
+                        requirements.apply {
+                            module.requirements().forEachIndexed { index, itemstack ->
+                                row(index)
+
+                                image(itemstack.item.icon(Cicon.medium))
+                                add(itemstack.item.localizedName, Pal.accent)
+                                add(" ${itemstack.amount}", Color.lightGray)
+                            }
+                        }
+                    }.pad(5f)
+
+                    row()
+
+                    line(5f, 3f, Pal.accent)
+
+                    row()
+
+                    table { level -> level.apply {
+                        add("Level: ", Pal.accent)
+                        add("${module.level}", Color.lightGray)
+                    }}.pad(5f)
+                }
+            }
+        }
+    }}
+
+    fun buildBars(table: Table) { table.apply {
+        module?.let { module ->
+            val modifiers = module.modifiers
+
+            modifiers.eachIndexed { index, prop ->
+                row(index)
+
+                add(Bar(prop.name, prop.color) {
+                    prop.value * module.level / prop.max
+                }).size(400f, 62.5f)
+            }
+        }
+    }}
 }
